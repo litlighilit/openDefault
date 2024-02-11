255e390a8320838ba0dfe2f579b99047117af91b
litlighilit <litlighilit@foxmail.com>
2024-02-11 16:01:38 +0800

    c/c++-version: code-refine and use *W over *A api

--- cpp/openDefaultBrowser.cpp
+++ cpp/openDefaultBrowser.cpp
@@ -1,52 +1,28 @@
 
 #include<shlwapi.h>
-#include<stdlib.h>
 
-#define assoc(buffer, sizep) AssocQueryStringA(ASSOCF_NOFIXUPS|ASSOCF_VERIFY, ASSOCSTR_EXECUTABLE, ext, NULL,buffer,sizep);
-extern "C"{
-#define presp(static_str) " " static_str
-const char* ext = ".html";
-}
+#pragma comment(lib, "Shlwapi.lib")
+#pragma comment(lib, "Shell32.lib")
 
-DWORD getDefaultBrowser(char* &nullbuffer){
+#define F_ASSOC ASSOCF_NONE // ASSOCF_NOFIXUPS|ASSOCF_VERIFY
+#define assoc(buffer, sizep) ::AssocQueryStringW(F_ASSOC,\
+  ASSOCSTR_EXECUTABLE, L".html", NULL, (buffer), (sizep))
+
+DWORD getDefaultBrowser(LPWSTR &pszPath){
     DWORD size = 0;
-    // first, get required buffer size into `size`
-	HRESULT hr = assoc(NULL, &size)
-	// assert(hr == S_FALSE);
-	
-    nullbuffer = new char[size];
-    hr = assoc(nullbuffer,&size);
-
-    // assert(hr==S_OK);
+    assoc(NULL, &size); // get required buffer size, returns S_FALSE
+    pszPath = new WCHAR[size];
+    HRESULT hr = assoc(pszPath,&size); // assert(hr==S_OK);
     return size;
-    // remember `delete[]nullbuffer;`
-	
+    // remember `delete[]pszPath;`
 }
-char* quotedArg(const char* arg){
-    size_t len = 2 + strlen(arg);
-    char* res = new char[len];
-    res[0] = '"';
-    strcpy(res+1, arg);
-    res[len-1] = '"';
-    return res;
-}
+
 int main(){
-    char* path;
-    DWORD size = getDefaultBrowser(path);
-    if (size == 0)return -1;
+    LPWSTR browserPath;
+    DWORD len = getDefaultBrowser(browserPath);
+    if (len == 0)return -1;
 
-    const char* qpath = quotedArg(path);
-    delete[] path;
-    size += 2;
+    ShellExecuteW(NULL, L"open", browserPath, L"about:blank", NULL, SW_SHOW);
 
-    const char* arg = presp("about:blank");
-
-    char* cmd = new char[size + strlen(arg) + 1];
-    strcpy(cmd, qpath);
-    strcat(cmd, arg);
-
-    system(cmd);
-
-    delete[] qpath;
-    delete[] cmd;
+    delete[] browserPath;
 }
